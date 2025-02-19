dfd57d90ac46e39fc090ed2f8d91803eb978cda3
swadha <swadhasiva@gmail.com>
2025-02-19 12:58:48 +0530

    Katalon 2

--- Test Suites/Visual Testing.ts
+++ Test Suites/Visual Testing.ts
@@ -14,7 +14,7 @@
    <testCaseLink>
       <guid>9295a856-7ba2-40bd-9e94-68f9e16962b5</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Visual Testing</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
@@ -23,6 +23,13 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Visual Testing 1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>20eb354a-676e-4871-87d9-42371621b58d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Account Creation</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
